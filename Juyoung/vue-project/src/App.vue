f3b48ee200a672e6fff7ceb5162e92bf1a40cd3a
ChoiJuyoung <cjy020718@gmail.com>
2022-05-27 00:12:33 +0900

    18. emit

--- Juyoung/vue-project/src/App.vue
+++ Juyoung/vue-project/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <TodoSimpleForm />
+    <TodoSimpleForm 
+      @add-todo="addTodo"
+    />
     <div v-if="!todos.length">
       추가된 Todo가 없습니다.
     </div>
     <div 
-      v-for="todo in todos" :key="todo.id"
+      v-for="(todo, index) in todos" :key="todo.id"
       class="card mt-2"
     >
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input 
             class="form-check-input" 
             type="checkbox"
@@ -22,6 +24,12 @@
           >
             {{ todo.subject }}
           </label>
+        </div>
+        <div>
+          <button 
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >Delete</button>
         </div>
       </div>
     </div>
@@ -37,34 +45,24 @@ export default {
     TodoSimpleForm
   },
   setup() {
-    const todo = ref('');
     const todos = ref([]);
-    const hasError = ref(false);
     const todoStyle = {
       textDecoration: 'line-through',
       color: 'gray'
     }
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    }
 
-    const onSubmit = () => {
-      if(todo.value === '') {
-        hasError.value = true;
-      } else {
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false,
-        });
-        hasError.value = false;
-        todo.value='';
-      }
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
     }
 
     return {
-      todo,
-      onSubmit,
+      addTodo,
       todos,
-      hasError,
-      todoStyle
+      todoStyle,
+      deleteTodo
     };
   }
 }
--- Juyoung/vue-project/src/components/TodoSimpleForm.vue
+++ Juyoung/vue-project/src/components/TodoSimpleForm.vue
@@ -18,14 +18,38 @@
           </button>
         </div>
       </div>
-      <div v-show="hasError" style="color: red">This field cannot be empty</div>
+      <div v-show="hasError" style="color:red">This field cannot be empty</div>
     </form>
 </template>
 
 <script>
-export default {
+  import { ref } from 'vue';
+  export default {
+    setup(props, context) {
+      const todo = ref('');
+      const hasError = ref(false);
+      const onSubmit = () => {
+        if(todo.value === '') {
+          hasError.value = true;
+        } else {
+          context.emit('add-todo', {
+            id: Date.now(),
+            subject: todo.value,
+            completed: false,
+          });
+          hasError.value = false;
+          todo.value='';
+        }
+      }
 
-}
+      return {
+        todo,
+        hasError,
+        onSubmit
+      }
+    }
+
+  }
 </script>
 
 <style>
