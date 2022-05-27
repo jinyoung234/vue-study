bac2a679365310024072065ee69e9b4eb9e2dd70
ChoiJuyoung <cjy020718@gmail.com>
2022-05-27 13:52:40 +0900

    24. 검색 기능 추가

--- Juyoung/vue-project/src/App.vue
+++ Juyoung/vue-project/src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="container">
-    <h4>count : {{ count }}</h4>
-    <h4>doublecount computed : {{ doubleCount }}</h4> 
-    <h4>doublecount computed : {{ doubleCount }}</h4>
-    <!-- computed 는 값을 저장하고 있기 때문에 한번 실행을 하고, 두번째 호출할때 그 값을 넣는다. -->
-    <h4>doubleCountMethod : {{ doubleCountMethod() }}</h4>
-    <h4>doubleCountMethod : {{ doubleCountMethod() }}</h4>
-    <!-- 함수는 값을 저장하고 있지 않기 때문에 호출을 두번하면 함수가 두 번 돌아간다. -->
-    <button @click="count++">add one</button>
-
     <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text" 
+      v-model="searchText"
+      placeholder="Search"
+    >
+    <hr />
     <TodoSimpleForm 
       @add-todo="addTodo"
     />
-    <div v-if="!todos.length">
-      추가된 Todo가 없습니다.
+    <div v-if="!filteredToods.length">
+      There is nothing to display
     </div>
     <TodoList 
-      :todos="todos" 
+      :todos="filteredToods" 
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
@@ -54,17 +52,16 @@ export default {
       console.log(todos.value[index].completed);
     }
 
-    const count = ref(1);
-    const doubleCount = computed(() => {
-      console.log('computed');
-      return count.value *2;
-      // computed 는 인수를 받을 수 없으며, 값이 변하면 실행된다.
+    const searchText = ref('');
+    const filteredToods = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      // empty string이면 모든 todo를 보여준다.
+      return todos.value;
     })
-
-    const doubleCountMethod = () => {
-      console.log('method');
-      return count.value*2;
-    }
 
     return {
       addTodo,
@@ -72,9 +69,8 @@ export default {
       todoStyle,
       deleteTodo,
       toggleTodo,
-      count,
-      doubleCount,
-      doubleCountMethod,
+      searchText,
+      filteredToods
     };
   }
 }
