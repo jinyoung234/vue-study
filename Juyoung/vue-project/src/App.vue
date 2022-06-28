1a5d4a0c080c7bfc2ddb0492a1f2c5eb8eac4973
ChoiJuyoung <cjy020718@gmail.com>
2022-06-28 23:13:49 +0900

    42. Navbar 추가하기

--- Juyoung/vue-project/src/App.vue
+++ Juyoung/vue-project/src/App.vue
@@ -1,5 +1,19 @@
 <template>
+<div>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">JUYOUNG</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#/todos">Todos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view/>
+</div>
 </template>
 
 <script>
