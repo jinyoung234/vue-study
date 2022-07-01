7cbd797326eef312ab08195dd297774cbe089de0
ChoiJuyoung <cjy020718@gmail.com>
2022-07-01 10:36:53 +0900

    52. To-Do 페이지 컴포넌트 만들기 4

--- Juyoung/vue-project/src/pages/todos/_id.vue
+++ Juyoung/vue-project/src/pages/todos/_id.vue
@@ -36,7 +36,11 @@
         </div>
       </div>
       
-      <button type="submit" class="btn btn-primary">
+      <button 
+        type="submit" 
+        class="btn btn-primary"
+        :disabled="!todoUpdated"
+      >
         Save
       </button>
       <button 
@@ -52,21 +56,30 @@
 <script>
 import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
-import { ref } from '@vue/reactivity';
+import { ref, computed } from 'vue';
+import _ from 'lodash';
 
 export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
     const todo = ref(null);
+    const originalTodo = ref(null);
     const loading = ref(true);
     const todoId = route.params.id;
+    
 
     const getTodo = async () => {
       const res = await axios.get(`http://localhost:3000/todos/${todoId}`)
-      todo.value = res.data;
+      todo.value = { ...res.data };
+      originalTodo.value = { ...res.data };
+
       loading.value = false;
     };
+
+    const todoUpdated = computed(() => {
+      return !_.isEqual(todo.value, originalTodo.value);
+    });
 
     const toggleTodoStatus = () => {
       todo.value.completed = !todo.value.completed;
@@ -86,7 +99,7 @@ export default {
         completed: todo.value.completed
       })
 
-      console.log(res);
+      originalTodo.value = {...res.data};
     }
 
     return {
@@ -95,6 +108,7 @@ export default {
       toggleTodoStatus,
       moveToTodoListPage,
       onSave,
+      todoUpdated,
     };
   }
 }
