d34a1ce0a9b4e11f1867d993107c4d8e629e91bc
ChoiJuyoung <cjy020718@gmail.com>
2022-07-01 13:20:24 +0900

    60. 폼 validation

--- Juyoung/vue-project/src/components/TodoForm.vue
+++ Juyoung/vue-project/src/components/TodoForm.vue
@@ -16,6 +16,12 @@
               type="text" 
               class="form-control"
             >
+            <div 
+              v-if="subjectError"
+              style="color: red"
+            >
+              {{ subjectError }}
+            </div>
           </div>
         </div>
         <div v-if="editing" class="col-6">
@@ -88,6 +94,7 @@ export default {
       completed: false,
       body: ''
     });
+    const subjectError = ref('');
     const originalTodo = ref(null);
     const loading = ref(false);
     const todoId = route.params.id;
@@ -132,6 +139,11 @@ export default {
     }
 
     const onSave = async () => {
+      subjectError.value = '';
+      if(!todo.value.subject) {
+        subjectError.value = 'Subject is required';
+        return;
+      }
       try{
         let res;
         const data = {
@@ -167,6 +179,7 @@ export default {
       toastAlertType,
       showToast,
       triggerToast,
+      subjectError
     };
   }
 }
